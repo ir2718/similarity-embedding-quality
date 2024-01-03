7631f912ecdf24c05707aa285ede1964daef4e93
ir2718 <irep2718@gmail.com>
2024-01-03 20:41:52 +0100

    remove scripts

--- script.sh
+++
@@ -1,3 +0,0 @@
-for ((i=0;i<=6;i++)); do
-    python -m src.scripts.training_stsbenchmark_hf --model_name google/electra-base-discriminator --model_load_path ./output/google-electra-base-discriminator_word_similarity_dataset/seed_0/pytorch_model.bin --num_seeds 1 --starting_state $i --pooling_fn mean --device cuda:1
-done
--- script3.sh
+++
@@ -1,3 +0,0 @@
-for ((i=0;i<=13;i++)); do
-    python -m src.scripts.training_stsbenchmark_hf --num_seeds 5 --model_name google/electra-base-discriminator --starting_state $i --pooling_fn max --device cuda:1
-done
